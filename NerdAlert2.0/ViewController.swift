45d1abdd1d3b8ae6ebe8ec061498179e512ba178
Ameer Bajwa <ameer.bajwa@revelsystems.com>
2024-07-22 14:40:24 -0700

    Linking github repo to project

---
+++ NerdAlert2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NerdAlert2.0
+//
+//  Created by Ameer Bajwa on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
